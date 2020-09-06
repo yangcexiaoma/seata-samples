e19cbe34e4e3055b29464d40e118e6fe6b68a177
yangce <281987330@qq.com>
2020-09-06 19:47:46 +0800

    Revert "事务版 AT"
    This reverts commit 6b58ac0d

--- seata-at/account/src/main/java/cn/tedu/account/service/AccountServiceImpl.java
+++ seata-at/account/src/main/java/cn/tedu/account/service/AccountServiceImpl.java
@@ -3,6 +3,7 @@ package cn.tedu.account.service;
 import cn.tedu.account.mapper.AccountMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigDecimal;
 
@@ -12,10 +13,15 @@ import java.math.BigDecimal;
  */
 @Service
 public class AccountServiceImpl implements AccountService{
+    @SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
     @Autowired
     private AccountMapper accountMapper;
     @Override
+    @Transactional
     public void decrease(Long userId, BigDecimal money) {
         accountMapper.decrease(userId,money);
+            if (Math.random() < 0.5) {
+            throw new RuntimeException("模拟异常");
+        }
     }
 }
